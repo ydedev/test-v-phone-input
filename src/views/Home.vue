4b2e3a322d6e663cc84ee9192df9fe9daa3c9595
Yang Deng <nicole75846@gmail.com>
2023-09-07 18:15:47 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,35 +8,6 @@ const phone = ref('');
 let phoneRef = ref()
 function submit(){
     console.log(phone)
-    console.log(value)
-    console.log(phoneRef.value)
-}
-function countryFunc(e){
-    console.log(e)
-    console.log( getCountryCodeForRegionCode( e ))
-}
-
-
-</script>
-
-<template>
-
-  <div style="margin: 0 auto; width: 430px;">
-    <v-phone-input ref="phoneRef" v-model="phone"  @update:country="countryFunc" />
-    <v-btn @click="submit" block class="mt-2">Submit</v-btn>
-  </div>
-
-</template><script setup>
-import { ref } from 'vue';
-import {
-	getCountryCodeForRegionCode,
-} from 'awesome-phonenumber'
-
-const phone = ref('');
-let phoneRef = ref()
-function submit(){
-    console.log(phone)
-    console.log(value)
     console.log(phoneRef.value)
 }
 function countryFunc(e){
